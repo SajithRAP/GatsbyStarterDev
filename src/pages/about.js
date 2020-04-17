8ba19fcf603f4c31947acfc3c6321f0f5abf3abd
SajithRAP <60466810+SajithRAP@users.noreply.github.com>
2020-04-17 17:35:44 +0530

    yml update 87

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
-// import React from "react"
+import React from "react"
 // import YAMLData from "../../content/about.yaml"
 
-// const YAMLbuildtime = () => (
-//   <div>
-//     <h1>{YAMLData.title}</h1>
-//     <h1>{YAMLData.intro}</h1>
-//   </div>
-// )
-// export default YAMLbuildtime
+const YAMLbuildtime = () => (
+  <div>
+    {/* <h1>{YAMLData.title}</h1>
+    <h1>{YAMLData.intro}</h1> */}
+    <p>Test</p>
+  </div>
+)
+export default YAMLbuildtime
